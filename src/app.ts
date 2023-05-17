dff1bb85eb2cffc71b57d5abeacc2f5ae3262244
Enow Divine <sirdivine16@gmail.com>
2023-05-17 22:08:43 +0100

    updates

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,6 @@ const http = require("http");
 const cors = require("cors");
 import bodyParser = require("body-parser");
 import { sendPushNotification } from "./services/push_notification/expo.notification";
-// socket import
-const socketIO = require("socket.io");
 
 import dbConnect from "./configs/db";
 import authAPI from "./apis/auth";
@@ -26,66 +24,6 @@ const corsOptions = {
   credentials: true,
   optionSuccessStatus: 200,
 };
-
-const PORT: any = process.env.PORT || 5000;
-const server = http.createServer(app);
-
-let users: { userId: any; socketId: any }[] = [];
-
-const addUser = (userId: any, socketId: any) => {
-  !users.some((user) => user.userId === userId) &&
-    users.push({ userId, socketId });
-};
-
-const removeUser = (socketId: any) => {
-  users = users.filter((user) => user.socketId !== socketId);
-};
-
-const getUser = (userId: any) => {
-  return users.find((user) => user.userId === userId);
-};
-
-const io = socketIO(server, {
-  cors: {
-    origin: "*",
-    credentials: true,
-    optionSuccessStatus: 200,
-  },
-});
-
-io.on("connection", (socket: any) => {
-  // when connected
-  console.log("user connected");
-
-  // get userId and ssocketId from user
-  socket.on("addUser", (userId: any) => {
-    addUser(userId, socket.id);
-  });
-
-  // send and get messages between users and admin
-  socket.on(
-    "sendMessage",
-    ({
-      senderId,
-      receiverId,
-      text,
-    }: {
-      senderId: any;
-      receiverId: any;
-      text: any;
-    }) => {
-      const user = getUser(receiverId);
-      const message = [senderId, receiverId];
-      io.to(user?.socketId).emit("getMessage", { message, text });
-    }
-  );
-
-  // when disconnected
-  socket.on("disconnect", () => {
-    console.log("a user is disconnected");
-    removeUser(socket.id);
-  });
-});
 
 app.use(cors(corsOptions));
 app.use(express.json());
@@ -124,6 +62,9 @@ app.post("/testPush", async (req: Request, res: Response) => {
     body: "test message",
   });
 });
+
+const PORT: any = process.env.PORT || 5000;
+const server = http.createServer(app);
 
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}, HURRAY!!!!!`);
